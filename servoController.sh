9cac7d1d3208de280654199ab427c6ab0ef17ad6
balj55 <byronamh@gmail.com>
2017-06-02 08:16:28 -0600

    shell

---
+++ servoController.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# framework - initializer
+
+sudo pigiod
+
+pigs s 21 1500
+echo "0deg completed";
+pigs s 21 1800
+echo "xdeg completed";
+
+pigs s 21 2000
+echo "ydeg completed";
+
+pigs s 21 2500
+echo "zdeg completed";
+
+
